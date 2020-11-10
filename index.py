5b28018c547b3f3475ed133e1cb48357ac5a5e5c
Edmy20 <74212389+Edmy20@users.noreply.github.com>
2020-11-09 22:57:39 -0600

    ruta

--- index.py
+++ index.py
@@ -210,4 +210,4 @@ def  home():
     return render_template('home.html', recetas=recetas)
 
 if __name__ =='__main__':
-    app.run(debug=True)
+    app.run(threaded=True, host="0.0.0.0", port=5000, debug=True)
